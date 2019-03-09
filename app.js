7998b5c2b55b17a407c993e2aa44fab6cb891fee
JOSE MIGUEL RAMIREZ MARTINEZ <JMRAMIREZM02@Puerto.liverpool.com.mx>
2019-03-09 08:32:02 -0600

    commented code removed

--- app.js
+++ app.js
@@ -14,13 +14,3 @@ app.listen(8081, () => {
     console.log('Nodemon is running the app on PORT:', 8081);
 });
 
-// module.exports = app;
-
-/*
-const noMutation = ["A T G C G A", "C A G T G C", "T T A T T T", "A G A C G G", "G C G T C A", "T C A C T G"];*/
-/*const mutation = ['A T G C G A', 'C A G T G C', 'T T A T G T', 'A G A A G G', 'C C C C T A', 'T C A C T G'];
-Helpers.sanitize(mutation)
-    .then(Helpers.diagonalToHorizontal)
-    .then(res => console.log(res))
-    .catch(err => console.error(err));*/
-
